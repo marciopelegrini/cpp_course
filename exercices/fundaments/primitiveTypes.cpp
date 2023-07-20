cd3497dada4de3dc9fdc7cb98112db582a1bf626
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2023-07-20 14:31:14 -0400

    add gitignore

---
+++ exercices/fundaments/primitiveTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool isAdmin = true; // Or 0 - 1
+    cout << isAdmin << endl;
+
+    char symbol('#');
+    cout << symbol << endl;
+
+
+    return 0;
+}
