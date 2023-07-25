9ac3c04fa20b79bbd3ada7c76ac051c2a7216fe3
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2023-07-25 14:28:25 -0400

    Section 5

---
+++ exercices/essential_concepts/tiny_project/src/main.cpp
@@ -0,0 +1,13 @@
+#include "functions/calc.h"
+#include <iostream>
+
+using namespace std;
+using namespace calc;
+
+int main()
+{
+    cout << add(10, 20) << endl;
+    cout << subtract(10, 20) << endl;
+
+    return 0;
+}
